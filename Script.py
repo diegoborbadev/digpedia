e866f6b628aeda2ad88a90f74a50686a795f0305
Diego Borba <diegoborba25.contato@gmail.com>
2023-12-21 15:24:23 -0300

    Script improved

--- Script.py
+++ Script.py
@@ -1,18 +1,28 @@
 import os
 
 if __name__ == "__main__":
+    # Walk through all directories and files
     for root, dirs, files in os.walk('.'):
+        # Ignore hidden directories
         if root.startswith('.\\.'):
             continue
         
+        # Filepath to summary file
         summary_file_path = f'{root}/Summary.md'
         with open(summary_file_path, "w") as summary_file:
+            # Add title
             summary_file.write("# Summary\n\n")
 
+            # Add directories to summary
             for dir in dirs:
+                # Ignore hidden directories
                 if not dir.startswith('.'):
-                    summary_file.write(f"- [{dir}]({dir}/Summary.md)\n")
+                    # Add to summary
+                    summary_file.write(f"- [{dir}]({dir}/Summary.md)\n") 
             
+            # Add files to summary
             for file in files:
+                # Ignore specific files
                 if not (file == 'Summary.md' or file == 'Script.py'):
-                    summary_file.write(f"- [{file}]({file})\n")
+                    filename = os.path.splitext(file)[0] # Remove extension
+                    summary_file.write(f"- [{filename}]({file})\n") # Add to summary
