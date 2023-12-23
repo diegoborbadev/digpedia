f0e22969bef8d18757b458cea137629dac5585e0
Diego Borba <diegoborba25.contato@gmail.com>
2023-12-23 12:17:16 -0300

    Script fixed

--- Script.py
+++ Script.py
@@ -8,28 +8,28 @@ if __name__ == "__main__":
         if root.startswith('./.'):
             continue
 
-        # Check if README.md is in files
-        if 'README.md' in files:
-            files.insert(0, files.pop(files.index('README.md')))
-
-        # Add files to summary
-        for file in files:
-            # Ignore specific files
-            if not (file == 'Summary.md' or file == 'Script.py'):
-                # Remove extension
-                filename = os.path.splitext(file)[0]
-
-                # Replace underscores with spaces
-                filename = filename.replace('_', ' ')
-
-                # Add to summary
-                summary_file.write(f"- [*{filename}*]({file})\n")
-
         # Filepath to summary file
         summary_file_path = f'{root}/Summary.md'
         with open(summary_file_path, "w") as summary_file:
             # Add title
             summary_file.write("# Summary\n\n")
+
+            # Check if README.md is in files
+            if 'README.md' in files:
+                files.insert(0, files.pop(files.index('README.md')))
+
+            # Add files to summary
+            for file in files:
+                # Ignore specific files
+                if not (file == 'Summary.md' or file == 'Script.py'):
+                    # Remove extension
+                    filename = os.path.splitext(file)[0]
+
+                    # Replace underscores with spaces
+                    filename = filename.replace('_', ' ')
+
+                    # Add to summary
+                    summary_file.write(f"- [*{filename}*]({file})\n")
 
             # Add directories to summary
             for dir in dirs:
